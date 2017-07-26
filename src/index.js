0d3ef60f89c69e066af7e0ebe8d397adcbb72164
Chiahao Lin <y80811@gmail.com>
2017-07-26 18:42:08 +0800

    feature-integration section is done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import registerServiceWorker from './registerServiceWorker';
 // style
 import 'normalize.css'
 import './styles/base.styl'
-import './styles/container.styl'
 import './styles/header.styl'
 import './styles/main.styl'
 
